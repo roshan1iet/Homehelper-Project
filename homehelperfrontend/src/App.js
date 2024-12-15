44805e28f9b5975dd788b3d7d9e08882799cbea8
roshan <roshan1iet@gmail.com>
2024-12-16 01:25:17 +0530

    Add missing files

--- homehelperfrontend/src/App.js
+++ homehelperfrontend/src/App.js
@@ -1,17 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Footer  from './components/Footer/Footer';
-import HomePage from './pages/HomePage.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <HomePage/>
-      <Footer/>
-    </div>
-  );
-}
-
-export default App;
+  import React from 'react';
+  import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+  
+  import logo from './logo.svg';
+  import './App.css';
+  import Navbar from './components/Navbar/Navbar';
+  import Footer  from './components/Footer/Footer';
+  import HomePage from './pages/Home.js';
+  import Payment from './pages/Payment.js';
+  
+  function App() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/payment" element={<Payment />} />
+            {/* Add other routes here */}
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+  
+  export default App;
+  
